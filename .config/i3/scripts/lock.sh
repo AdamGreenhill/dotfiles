a569c336fefad036d6aef2349f7300ef63b5ee61
Adam Greenhill <adam.greenhill@gmail.com>
2016-01-26 23:49:06 -0500

    final lock

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#scrot /tmp/screen.png -e 'convert -scale 5% -scale 2000% $f /tmp/screen.png'
-i3lock -u #-i /tmp/screen.png
-#rm /tmp/screen.png
+scrot /tmp/screen.png -e 'convert -blur 5x3 $f /tmp/screen.png'
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
